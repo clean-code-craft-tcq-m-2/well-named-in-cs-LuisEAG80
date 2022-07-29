37e0c34f2609a98b9a44e5570173eeec71ce17fa
Luis Enrique Acosta <acl1ga@bosch.com>
2022-07-29 02:03:08 -0500

    Adding enums for major and minor colors

---
+++ TelCo.ColorCoder/Enums/MinorColor.cs
@@ -0,0 +1,11 @@
+﻿namespace TelCo.ColorCoder.Enums
+{
+    public enum MinorColor
+    {
+        Blue = 0,
+        Orange = 1,
+        Green = 2,
+        Brown = 3,
+        SlateGray = 4,
+    }
+}
